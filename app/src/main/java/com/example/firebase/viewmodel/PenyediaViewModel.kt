973281b28f9227a1b178976f45e71ef25a6b95fa
Naufal0067 <naufalopang1928@gmail.com>
2026-01-08 05:36:25 +0700

    membuat PenyediaViewModel.kt untuk memanggil view model

---
+++ app/src/main/java/com/example/firebase/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.firebase.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+    }
+}
