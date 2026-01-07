25dadb61e4f5b55162b00f2381f68c9e7d957964
Naufal0067 <naufalopang1928@gmail.com>
2026-01-08 05:22:41 +0700

    menambah override untuk post data siswa

--- app/src/main/java/com/example/firebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/firebase/repositori/RepositorySiswa.kt
@@ -32,3 +32,14 @@ class FirebaseRepositorySiswa : RepositorySiswa {
         }
     }
 
+    override suspend fun postDataSiswa(siswa: Siswa) {
+        val docRef = collection.document(siswa.id.toString())
+        val data = hashMapOf(
+            "id" to siswa.id,
+            "nama" to siswa.nama,
+            "alamat" to siswa.alamat,
+            "telpon" to siswa.telpon
+        )
+        docRef.set(data).await()
+    }
+
