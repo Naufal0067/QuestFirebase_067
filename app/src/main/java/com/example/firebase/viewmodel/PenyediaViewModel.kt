823a21606c8f55f2278fc7ab20d10ad99a1d29dc
Naufal0067 <naufalopang1928@gmail.com>
2026-01-08 12:08:22 +0700

    menambahkan penyedia view model untuk detail view model

--- app/src/main/java/com/example/firebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/PenyediaViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.firebase.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.firebase.repositori.AplikasiDataSiswa
 
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
@@ -12,5 +14,12 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+
     }
 }
