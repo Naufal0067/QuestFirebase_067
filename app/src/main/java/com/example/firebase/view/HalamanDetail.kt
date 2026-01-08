99205d65510ce8b3c60b0f2da1d20415cc0ddf8f
Naufal0067 <naufalopang1928@gmail.com>
2026-01-08 11:52:02 +0700

    membuat code untuk memmunculkan konfirmasi hapus data

--- app/src/main/java/com/example/firebase/view/HalamanDetail.kt
+++ app/src/main/java/com/example/firebase/view/HalamanDetail.kt
@@ -120,3 +120,17 @@ private fun BodyDetailSiswa(
         ) {
             Text(stringResource(R.string.delete))
         }
+
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = { deleteConfirmationRequired = false },
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+    }
+}
+
